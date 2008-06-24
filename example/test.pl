099c06d7a657e9a9a1bf6170bed6eb53fdbfeb8b
Jonathan Rockway <jon@jrock.us>
2008-06-24 11:11:54 -0500

    commit it!

---
+++ example/test.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Your::Brain;
+use A::CPAN::Module;
--- lib/Devel/WhatDidIForget.pm
+++ lib/Devel/WhatDidIForget.pm
@@ -1,11 +1,55 @@
 package Devel::WhatDidIForget;
 use strict;
 use warnings;
+use IO::Scalar;
+
+sub DB::DB { warn 'DB' };
+
+my @forgotten;
+
+BEGIN {
+    push @INC, sub {
+        my $mod = $_[1];
+        $mod =~ s/[.]pm$//g;
+        $mod =~ s/\W/::/g;
+
+        push @forgotten, $mod;
+
+        my $first = 1;
+        return sub { 
+            do { $_ = "1" } and return 1 if $first--; 
+            return 0;
+        };
+    };
+}
+
+CHECK {
+    die "You are missing: ", join ', ', @forgotten if @forgotten;
+}
+
+1;
 
 =head1 NAME
 
-Devel::WhatDidIForget - 
+Devel::WhatDidIForget - what modules did I forget to install?
+
+=head1 SYNOPSIS
+
+  perl -d:WhatDidIForget some_script.pl
+
+=head1 DESCRIPTION
+
+Sometimes you forget to declare your prerequisites and have to
+painfully run your program multiple times, watch it print C<<Can't
+locate Foo.pm in @INC (@INC contains ...)>>, install that module, and
+repeat until you have everything installed.  That is tedious and time
+consuming.  You can't even paste the module in the error message into
+the CPAN shell; you have to translate C</>s to C<::>s.
+
+This module can help.  It will catch all the uninstalled modules,
+print them (in pastable form), and then exit.
 
 =cut
 
+__DATA__
 1;
